54a18ebe2c70c95b127f2f98cb5fa16d2a14a619
madison sorah <madisonsorah@gmail.com>
2021-11-06 20:19:05 -0400

    commit tree f3e6fdd2fdf66c89f461ee6d28ee0367070e915b

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// New variable totalBatteries holds the sum of each battery amount in batteryBatches
+const totalBatteries = batteryBatches.reduce(function(sum, element) {
+    return element + sum}, 0);
